d21d7164d4b0fe9a7dcb51c9f42e711c2c1862ad
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-08-24 09:45:46 -0400

    Add Factory Method

---
+++ src/creation/factory_method.ts
@@ -0,0 +1,36 @@
+export interface Channel {
+
+    send(user: string, message: string): Promise<void>;
+
+}
+
+export class EmailChannel implements Channel {
+
+    public async send(user: string, message: string): Promise<void> {
+        console.log(`Email Message: [${user}] -> ${message}`);
+    }
+
+}
+
+export class ChatChannel implements Channel {
+
+    public async send(user: string, message: string): Promise<void> {
+        console.log(`Chat Message: [${user}] -> ${message}`);
+    }
+
+}
+
+// ChannelFactory.createNotification('Chat')
+// ChannelFactory.createNotification('Email')
+
+export class ChannelFactory {
+
+    public static createNotification(type: 'Chat' | 'Email') {
+        switch (type) {
+            case 'Chat':
+                return new ChatChannel();
+            case 'Email':
+                return new EmailChannel();
+        }
+    }
+}
