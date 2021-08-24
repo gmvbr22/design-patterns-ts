b116415a0b9c73e0d993e1a6362c42d97c9b5423
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-08-24 12:39:09 -0400

    Run Lint Fix

--- src/creation/factory_method.ts
+++ src/creation/factory_method.ts
@@ -1,36 +1,29 @@
 export interface Channel {
-
-    send(user: string, message: string): Promise<void>;
-
+  send(user: string, message: string): Promise<void>;
 }
 
 export class EmailChannel implements Channel {
-
-    public async send(user: string, message: string): Promise<void> {
-        console.log(`Email Message: [${user}] -> ${message}`);
-    }
-
+  public async send(user: string, message: string): Promise<void> {
+    console.log(`Email Message: [${user}] -> ${message}`);
+  }
 }
 
 export class ChatChannel implements Channel {
-
-    public async send(user: string, message: string): Promise<void> {
-        console.log(`Chat Message: [${user}] -> ${message}`);
-    }
-
+  public async send(user: string, message: string): Promise<void> {
+    console.log(`Chat Message: [${user}] -> ${message}`);
+  }
 }
 
 // ChannelFactory.createNotification('Chat')
 // ChannelFactory.createNotification('Email')
 
 export class ChannelFactory {
-
-    public static createNotification(type: 'Chat' | 'Email'): Channel {
-        switch (type) {
-            case 'Chat':
-                return new ChatChannel();
-            case 'Email':
-                return new EmailChannel();
-        }
+  public static createNotification(type: 'Chat' | 'Email'): Channel {
+    switch (type) {
+      case 'Chat':
+        return new ChatChannel();
+      case 'Email':
+        return new EmailChannel();
     }
+  }
 }
